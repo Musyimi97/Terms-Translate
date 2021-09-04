35ff36bba62302518680dee6707fb23ae45abaa1
Musyimi97 <collinsmusyimi.cm@gmail.com>
2021-09-04 14:36:49 +0300

    addign commandline options

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,13 @@
 #! /usr/bin/env node
-console.log("Hello World!");
+const yargs = require("yargs");
+
+const usage = "\nUsage: tran <lang_name> sentence to be translated";
+const options = yargs
+  .usage(usage)
+  .option("l", {
+    alias: "languages",
+    describe: "List all supported languages.",
+    type: "boolean",
+    demandOption: false,
+  })
+  .help(true).argv;
